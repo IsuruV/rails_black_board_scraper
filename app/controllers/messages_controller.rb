9f42fe18c1aad7bf59b2466b8c447e3cc69199b5
IsuruV <isuru260@gmail.com>
2017-04-06 00:13:20 +0000

    format messages

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,14 +4,23 @@ class MessagesController < ApplicationController
         if message.save
             ActionCable.server.broadcast 'messages',
             message: message.content,
-            user: message.user.formatted_user_message
+            user: message.user
             head :ok
         end
+    end
+    
+    def index
+        # require 'pry'; binding.pry
+        @messages = Message.all.where(classroom_id: message_all_params)
+        render json: {"messages": @messages.map{|message| message.message_format}}
     end
     
 private
     def message_params
       params.require(:message).permit(:content, :chatroom_id, :user_id)
+    end
+    def message_all_params
+        params.require(:class_id)
     end
     
 end
