391c9e88a9e25de9d5b5a59dec09208091b53c9a
IsuruV <isuru260@gmail.com>
2017-04-11 00:48:13 +0000

    association error

--- app/models/classroom.rb
+++ app/models/classroom.rb
@@ -3,5 +3,5 @@ class Classroom < ActiveRecord::Base
     has_many :users, :through => :user_classrooms
     has_many :rosters
     has_many :messages
-    has_many :users, through: :messages
+    # has_many :users, through: :messages
 end
